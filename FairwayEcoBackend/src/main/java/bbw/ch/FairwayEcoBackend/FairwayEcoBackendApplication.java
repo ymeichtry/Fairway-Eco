d8975734f9fc3497edb55829d3447c15ec5a3223
Oliver Zenger <oliver@zenger.org>
2025-12-04 16:15:17 +0100

    Initial Spring Boot setup

---
+++ FairwayEcoBackend/src/main/java/bbw/ch/FairwayEcoBackend/FairwayEcoBackendApplication.java
@@ -0,0 +1,13 @@
+package bbw.ch.FairwayEcoBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FairwayEcoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FairwayEcoBackendApplication.class, args);
+	}
+
+}
