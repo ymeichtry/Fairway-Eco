ac54149cbf227b500f8fbb8fd54a3ee2e535d9e7
Yanis Meichtry <yanis@meichtry.dev>
2026-01-15 11:15:01 +0100

    linter should work now

--- FairwayEcoFrontend/tailwind.config.ts
+++ FairwayEcoFrontend/tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import tailwindcssAnimate from "tailwindcss-animate";
 
 export default {
   darkMode: ["class"],
@@ -135,5 +136,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 } satisfies Config;
