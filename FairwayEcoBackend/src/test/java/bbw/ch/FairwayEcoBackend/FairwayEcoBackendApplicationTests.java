fe33c76a0b299f343980e74182d2a597559518a1
Yanis Meichtry <yanis@meichtry.dev>
2026-01-15 11:21:24 +0100

    added frontend url that the backend works

--- FairwayEcoBackend/src/test/java/bbw/ch/FairwayEcoBackend/FairwayEcoBackendApplicationTests.java
+++ FairwayEcoBackend/src/test/java/bbw/ch/FairwayEcoBackend/FairwayEcoBackendApplicationTests.java
@@ -2,8 +2,10 @@ package bbw.ch.FairwayEcoBackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 
 @SpringBootTest
+@EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:9092", "port=9092"})
 class FairwayEcoBackendApplicationTests {
 
 	@Test
